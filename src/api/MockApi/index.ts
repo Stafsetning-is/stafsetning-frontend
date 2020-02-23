eb820cbdf1692cde7b1e3e97b9c3742e4a514d78
Njáll Skarphéðinsson <njallskarp>
2020-02-23 11:45:12 +0000

    added func

--- src/api/MockApi/index.ts
+++ src/api/MockApi/index.ts
@@ -1,1 +1,30 @@
-export const getExerciseById = (id: string) => {};
+/**
+ * retuns an array of sentence parts
+ * @param id redundant id
+ */
+export const getExerciseById = (id: string) => {
+	return delay([
+		"Fiskar hafa synt um heimsins höf",
+		"og vötn í meira en",
+		"500 milljónir ára.",
+		"Fyrstu dýrin, sem hægt var að kalla",
+		"þessu nafni, voru þó hvorki með",
+		"hreistur, ugga né kjálka",
+		"og því mjög frábrugðin",
+		"þeim sem við þekkjum til í dag",
+		"undir sama heiti.",
+		"Áður fyrr hafði þetta hugtak",
+		"lika mun víðtækari merkingu."
+	]);
+};
+
+/**
+ * Returns a promise of data
+ * that will resolve in 1250ms
+ * @param data Data to return after delay
+ */
+const delay = (data: any) => {
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(data), 1250);
+	});
+};
