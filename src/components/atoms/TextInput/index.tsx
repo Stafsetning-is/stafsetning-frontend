3de7388c4cd1210761184f890d3826259659d7ba
Kjartan97 <kjartan.reynir.olafsson@gmail.com>
2020-03-01 13:01:21 +0000

    some minor styling effort

--- src/components/atoms/TextInput/index.tsx
+++ src/components/atoms/TextInput/index.tsx
@@ -1,25 +1,28 @@
 import * as React from "react";
 import { InputComponent } from "../../../services";
+import { TextInputTitle, TextInputField } from "./styles";
 
 /**
  * Text input that takes in value and onChange function
  * This is for the form functionality
  */
 export const TextInput = ({
-	value,
-	onChange,
-	label,
-	placeholder
+    value,
+    onChange,
+    label,
+    placeholder
 }: InputComponent) => {
-	return (
-		<React.Fragment>
-			<p>{label}</p>
-			<input
-				type="text"
-				value={value}
-				onChange={(e) => onChange(e.target.value)}
-				placeholder={placeholder}
-			/>
-		</React.Fragment>
-	);
+    return (
+        <React.Fragment>
+            <TextInputTitle>{label}</TextInputTitle>
+            <TextInputField>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
+                    placeholder={placeholder}
+                />
+            </TextInputField>
+        </React.Fragment>
+    );
 };
---
+++ src/components/atoms/TextInput/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const TextInputTitle = styled.p`
+    color: gray;
+    font-size: 13px;
+`;
+export const TextInputField = styled.div`
+    left: 5px;
+    color: gray;
+    width: 40px;
+`;
