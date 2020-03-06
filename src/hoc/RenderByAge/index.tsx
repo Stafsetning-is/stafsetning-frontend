21f2d962e64d310bb3074b3d65519297f8cecd9b
Njáll Skarphéðinsson <njallskarp>
2020-03-06 12:45:43 +0000

    added render by age

--- src/hoc/RenderByAge/index.tsx
+++ src/hoc/RenderByAge/index.tsx
@@ -1,8 +1,6 @@
-import { FC } from "react";
-
 const userAge = 13;
 
-export const RenderByAge = (Component: FC, minimumAge: number) => {
+export const RenderByAge = (Component: any, minimumAge: number) => {
 	if (userAge < minimumAge) return () => null;
 	return Component;
 };
