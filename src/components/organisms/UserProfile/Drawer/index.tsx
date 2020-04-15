76a4a0fd3ead80bf0509045dab9dfaaf78770fa2
nannalop <natalia17@ru.is>
2020-04-15 12:30:43 +0000

    buttons added to added without navigation

--- src/components/organisms/UserProfile/Drawer/index.tsx
+++ src/components/organisms/UserProfile/Drawer/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Frame } from "./styles";
+import { Frame, Button } from "./styles";
 /* Should contain a line and under the line we have a two clickable options */
 
 export default () => {
-    return <Frame></Frame>;
+    return (
+        <Frame>
+            <Button>Kláraðar æfingar</Button>
+            <Button>Vistaðar æfingar</Button>
+        </Frame>
+    );
 };
