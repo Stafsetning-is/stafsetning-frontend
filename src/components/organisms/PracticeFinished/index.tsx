28e7051a3a012c0d7ec2e9c9db8204af72f35171
kjartan <kjartan.reynir.olafsson@gmail.com>
2020-04-15 16:01:47 +0000

    removed time div in PracticeFinished and connected PracticeText

--- src/components/organisms/PracticeFinished/index.tsx
+++ src/components/organisms/PracticeFinished/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { IProps } from "./interface";
 import {
-	TopBar,
-	TitleText,
-	TitleElement,
-	SecondaryTitle,
-	OverviewContainer,
-	SuggestionTitle,
-	Outer,
-	ErrorCount,
-	TimeText
+    TopBar,
+    TitleText,
+    TitleElement,
+    SecondaryTitle,
+    OverviewContainer,
+    SuggestionTitle,
+    Outer,
+    ErrorCount,
+    TimeText,
 } from "./styles";
 import { ExerciseBoxesContainer } from "../../";
 import TryTable from "./TryTable";
@@ -18,43 +18,43 @@ import { StoreState } from "../../../reducers";
 import { durationToTime } from "./utils";
 import Moment from "moment";
 import "moment/locale/is";
+import PracticeText from "./PracticeText";
 const Component = ({
-	nextUp,
-	exercise: { title },
-	errorItems,
-	duration,
-	createdAt
+    nextUp,
+    exercise: { title },
+    errorItems,
+    duration,
+    createdAt,
 }: IProps) => {
-	const [mm, ss] = durationToTime(duration);
-	Moment.locale("is");
-	const test = Moment(createdAt).fromNow();
-	return (
-		<React.Fragment>
-			<TitleText>
-				<TitleElement>{title}</TitleElement>
-			</TitleText>
-			<SecondaryTitle>
-				<TitleElement>{test}</TitleElement>
-			</SecondaryTitle>
-			<TopBar>
-				<p>Vel gert! Æfingin skapar meistarann</p>
-			</TopBar>
-			<OverviewContainer>
-				<Outer>
-					<ErrorCount>Þú gerðir {errorItems.length} Villur</ErrorCount>
-					<TimeText>
-						Tíminn þinn var {mm}:{ss}
-					</TimeText>
-				</Outer>
-			</OverviewContainer>
-			<SuggestionTitle>Haltu áfram að æfa þig!</SuggestionTitle>
-			<ExerciseBoxesContainer exercises={nextUp} />
-		</React.Fragment>
-	);
+    const [mm, ss] = durationToTime(duration);
+    Moment.locale("is");
+    const test = Moment(createdAt).fromNow();
+    return (
+        <React.Fragment>
+            <TitleText>
+                <TitleElement>{title}</TitleElement>
+            </TitleText>
+            <SecondaryTitle>
+                <TitleElement>{test}</TitleElement>
+            </SecondaryTitle>
+            <TopBar>
+                <p>Vel gert! Æfingin skapar meistarann</p>
+            </TopBar>
+            <OverviewContainer>
+                <Outer>
+                    <ErrorCount>
+                        Þú gerðir {errorItems.length} Villur
+                    </ErrorCount>
+                </Outer>
+            </OverviewContainer>
+            <SuggestionTitle>Haltu áfram að æfa þig!</SuggestionTitle>
+            <ExerciseBoxesContainer exercises={nextUp} />
+        </React.Fragment>
+    );
 };
 
 const mapStateToProps = (state: StoreState) => ({
-	nextUp: state.exercises.selection
+    nextUp: state.exercises.selection,
 });
 
 export const PracticeFinished = connect(mapStateToProps)(Component);
