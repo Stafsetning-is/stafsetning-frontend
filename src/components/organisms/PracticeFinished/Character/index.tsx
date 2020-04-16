2e137018bb6d1894fff642794c13f255da6de557
kjartan <kjartan.reynir.olafsson@gmail.com>
2020-04-16 10:02:28 +0000

    fixed styling and added comments

--- src/components/organisms/PracticeFinished/Character/index.tsx
+++ src/components/organisms/PracticeFinished/Character/index.tsx
@@ -10,20 +10,36 @@ import { CharSpan, ErrSpan, TypedError } from "./styles";
 export default ({ char, error }: IProps) => {
     const [showError, setShowError] = useState(false);
 
+    /**here the error highlight will be
+     * predefined as common mistakes made with symbols */
+    if (error === " ") error = "óþarfa bil";
+    if (error === ".") error = "óþarfa punktur";
+    if (error === ",") error = "óþarfa komma";
+    // var regexpAlphabetChar = new RegExp(/^[a-zA-Z]/);
+    // if (error) {
+    //     var errorChar = error.charAt(0);
+    //     if (!regexpAlphabetChar.test(errorChar)) {
+    //         error = "hvad gerdis her?";
+    //     }
+    // }
+
     /**
      * here if the error is null or essentially not a erro it will receive a null value
      */
-    if ({ error } === null) {
-        const typedError = null;
-    }
-    const typedError = <TypedError>{error}</TypedError>;
+    const typedError = showError ? <TypedError>{error}</TypedError> : null;
 
     /** However if the supposed error truly is an error, the if(error) will be true
      * and will return the character and the wrongly typed character aswell
      */
     if (error)
         return (
-            <ErrSpan>
+            /**
+             * here the onMouse will show the error message
+             */
+            <ErrSpan
+                onMouseEnter={() => setShowError(true)}
+                onMouseLeave={() => setShowError(false)}
+            >
                 {char}
                 {typedError}
             </ErrSpan>
