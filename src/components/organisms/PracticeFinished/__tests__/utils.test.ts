3af3e973191ce3eb954d9443e696841a9199ec36
kjartan <kjartan.reynir.olafsson@gmail.com>
2020-04-17 18:56:09 +0000

    added 5 tests for feedback error strings

--- src/components/organisms/PracticeFinished/__tests__/utils.test.ts
+++ src/components/organisms/PracticeFinished/__tests__/utils.test.ts
@@ -1,7 +1,48 @@
 import { getFeedback } from "../utils";
 describe("Error feedback based on error", () => {
-	it("Should give correct string with 2 errors", () => {
-		const string = getFeedback(2);
-		expect(string).toBe("2 villur! Vel Gert!");
-	});
+    it("Should give correct string with 2 errors", () => {
+        const string = getFeedback(2);
+        expect(string).toBe("2 villur! Vel Gert!");
+    });
+
+    describe("Error feedback based on error", () => {
+        it("Should give correct string with 1 error", () => {
+            const string = getFeedback(1);
+            expect(string).toBe("1 villa! Næstum því fullkomið. Vel Gert!");
+        });
+    });
+
+    describe("Error feedback based on error", () => {
+        it("Should give correct string with 3 errors", () => {
+            const string = getFeedback(3);
+            expect(string).toBe("3 villur! Vel Gert!");
+        });
+    });
+
+    describe("Error feedback based on error", () => {
+        it("Should give correct string with 21 errors", () => {
+            const string = getFeedback(21);
+            expect(string).toBe(
+                "21 villa! Þetta hefði mátt fara betur. Prufaðu aftur..."
+            );
+        });
+    });
+
+    describe("Error feedback based on error", () => {
+        it("Should give correct string with 20 errors", () => {
+            const string = getFeedback(20);
+            expect(string).toBe(
+                "20 villur! Þetta hefði mátt fara betur. Prufaðu aftur..."
+            );
+        });
+    });
+
+    describe("Error feedback based on error", () => {
+        it("Should give correct string with 11 errors", () => {
+            const string = getFeedback(11);
+            expect(string).toBe(
+                "11 villur! Þetta hefði mátt fara betur. Prufaðu aftur..."
+            );
+        });
+    });
 });
