9c3659e9c89f936bd3b23f4d6557a468b4aeb5a5
njall skarphedinsson <njallis@gmail.com>
2020-04-22 21:33:03 +0000

    counts grammar rules

--- src/components/organisms/FilterSearch/GrammarRuleField/index.tsx
+++ src/components/organisms/FilterSearch/GrammarRuleField/index.tsx
@@ -1,8 +1,32 @@
 import React, { useMemo } from "react";
 import { connect } from "react-redux";
-import { IProps } from "./interface";
+import { IProps, GrammarRule } from "./interface";
 import { StoreState } from "../../../../reducers";
 
-export default ({ exercises }: IProps) => {
-	return <div>ng og nk</div>;
+const Component = ({ exercises }: IProps) => {
+	const rules = useMemo(() => {
+		const rules = exercises.reduce<GrammarRule[]>((prev, { report }) => {
+			const rules = Object.keys(report).map((key) => ({
+				...report[key],
+				id: key,
+			}));
+			return [...prev, ...rules];
+		}, []);
+		const ruleObject: { [key: string]: GrammarRule } = {};
+		rules.forEach((rule) => {
+			if (!ruleObject[rule.id]) {
+				ruleObject[rule.id] = rule;
+				return;
+			}
+			ruleObject[rule.id].count++;
+		});
+		return Object.keys(ruleObject).map((key) => ruleObject[key]);
+	}, [exercises]);
+	return <div>{JSON.stringify(rules)}</div>;
 };
+
+const mapStateToProps = (state: StoreState) => ({
+	exercises: state.exercises.selection,
+});
+
+export default connect(mapStateToProps)(Component);
--- src/components/organisms/FilterSearch/GrammarRuleField/interface.ts
+++ src/components/organisms/FilterSearch/GrammarRuleField/interface.ts
@@ -1,5 +1,13 @@
-import { Exercise } from "../../../../models";
+import { Exercise, Report as _Report } from "../../../../models";
 
 export interface IProps {
 	exercises: Exercise[];
 }
+
+export interface GrammarRule {
+	count: number;
+	name: string;
+	id: string;
+}
+
+export interface Report extends _Report {}
