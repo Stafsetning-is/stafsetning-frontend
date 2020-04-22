9cf5bd693e31ac72cc1711cc42f2800167cd1b36
nannalop <natalia17@ru.is>
2020-04-22 17:07:10 +0000

    added page protection so only users can get on /user/settings

--- src/components/organisms/FormGenerator/LiveForm/index.tsx
+++ src/components/organisms/FormGenerator/LiveForm/index.tsx
@@ -5,50 +5,55 @@ import { TopErrorLabel, Form } from "../styles";
 import { InputElementContainer } from "./styles";
 import { getLiveInputElementsArray } from "../utils";
 import { IProps } from "./interface";
+import { ProtectPageWrapper } from "../../../../hoc";
 
 export const LiveForm = <T extends {}>({
-	fields,
-	postTo,
-	onSuccess,
+    fields,
+    postTo,
+    onSuccess,
 }: IProps<T>) => {
-	const [formObject, setFormObject] = useState(fields);
-	const [errorMessage, setErrorMessage] = useState("");
-	const inputElements = getLiveInputElementsArray(formObject);
+    const [formObject, setFormObject] = useState(fields);
+    const [errorMessage, setErrorMessage] = useState("");
+    const inputElements = getLiveInputElementsArray(formObject);
 
-	const handleChange = (key: keyof typeof fields, val: any) => {
-		const formObjectCopy = { ...formObject };
-		const formElementCopy = { ...formObjectCopy[key] };
-		formElementCopy.value = val;
-		formElementCopy.modified = true;
-		formObjectCopy[key] = formElementCopy;
-		setFormObject({ ...formObjectCopy });
-		setTimeout(handleChangeElementsToNotModified, 1000);
-	};
+    const handleChange = (key: keyof typeof fields, val: any) => {
+        const formObjectCopy = { ...formObject };
+        const formElementCopy = { ...formObjectCopy[key] };
+        formElementCopy.value = val;
+        formElementCopy.modified = true;
+        formObjectCopy[key] = formElementCopy;
+        setFormObject({ ...formObjectCopy });
+        setTimeout(handleChangeElementsToNotModified, 1000);
+    };
 
-	const handleChangeElementsToNotModified = () => {
-		const formObjectCopy = { ...formObject };
-		for (const key in formObjectCopy) {
-			const formElementCopy = { ...formObjectCopy[key] };
-			formElementCopy.modified = false;
-			formObjectCopy[key] = formElementCopy;
-		}
-		setFormObject({ ...formObjectCopy });
-	};
+    const handleChangeElementsToNotModified = () => {
+        const formObjectCopy = { ...formObject };
+        for (const key in formObjectCopy) {
+            const formElementCopy = { ...formObjectCopy[key] };
+            formElementCopy.modified = false;
+            formObjectCopy[key] = formElementCopy;
+        }
+        setFormObject({ ...formObjectCopy });
+    };
 
-	return (
-		<LayoutWrapper>
-			<TopErrorLabel>{errorMessage}</TopErrorLabel>
-			<Form onSubmit={(e) => e.preventDefault()}>
-				{inputElements.map((element) => (
-					<InputElementContainer>
-						<span>{element.modified ? "🤔" : "✅"}</span>
-						<InputFactory
-							{...element}
-							onChange={(val) => handleChange(element.key, val)}
-						/>
-					</InputElementContainer>
-				))}
-			</Form>
-		</LayoutWrapper>
-	);
+    return (
+        <ProtectPageWrapper>
+            <LayoutWrapper>
+                <TopErrorLabel>{errorMessage}</TopErrorLabel>
+                <Form onSubmit={(e) => e.preventDefault()}>
+                    {inputElements.map((element) => (
+                        <InputElementContainer>
+                            <span>{element.modified ? "🤔" : "✅"}</span>
+                            <InputFactory
+                                {...element}
+                                onChange={(val) =>
+                                    handleChange(element.key, val)
+                                }
+                            />
+                        </InputElementContainer>
+                    ))}
+                </Form>
+            </LayoutWrapper>
+        </ProtectPageWrapper>
+    );
 };
