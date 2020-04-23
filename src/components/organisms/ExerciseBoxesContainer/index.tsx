391ed5d5ff1bc845abef5a1b414fe0f75a1aeb8c
njall skarphedinsson <njallis@gmail.com>
2020-04-23 17:18:11 +0000

    connected to redux and added action

--- src/components/organisms/ExerciseBoxesContainer/index.tsx
+++ src/components/organisms/ExerciseBoxesContainer/index.tsx
@@ -2,17 +2,27 @@ import React from "react";
 import { ExerciseBox, LoaderBox } from "../../";
 import { Container } from "./styles";
 import { IProps } from "./interface";
+import { connect } from "react-redux";
+import { starExerciseInSelection } from "../../../actions";
 
-export const ExerciseBoxesContainer = ({ exercises, limit }: IProps) => {
+const Component = ({ exercises, limit, starExerciseInSelection }: IProps) => {
 	const sliceAt = limit ?? exercises.length;
 
 	return (
 		<LoaderBox loading={exercises.length === 0}>
 			<Container>
 				{exercises.slice(0, sliceAt).map((itm) => (
-					<ExerciseBox {...itm} />
+					<ExerciseBox
+						{...itm}
+						key={`${itm._id}:${itm.saved}`}
+						onStarClick={starExerciseInSelection}
+					/>
 				))}
 			</Container>
 		</LoaderBox>
 	);
 };
+
+export const ExerciseBoxesContainer = connect(null, {
+	starExerciseInSelection,
+})(Component);
