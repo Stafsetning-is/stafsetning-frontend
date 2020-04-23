41d5bb58ea00fd32b4382541388182c52291a7b4
njall skarphedinsson <njallis@gmail.com>
2020-04-23 12:02:44 +0000

    cwt

--- src/components/organisms/FilterSearch/interface.ts
+++ src/components/organisms/FilterSearch/interface.ts
@@ -1,15 +1,6 @@
-import { QuickFilter } from "../../../services";
+import { FilterObject, QuickFilter } from "../../../services";
 
-export interface IProps {
-	setHideCompleted: (shouldHide: boolean) => void;
-	setMaxWordCount: (newValue: number) => void;
-	setMinWordCount: (newValue: number) => void;
-	setQuickFilter: (filter: QuickFilter) => void;
-	minWordCount: number;
-	maxWordCount: number;
-	hideCompleted: boolean;
-	quickFilter: QuickFilter;
-}
+export interface IProps extends FilterObject {}
 
 export interface FilterButton {
 	quickFilter: QuickFilter;
