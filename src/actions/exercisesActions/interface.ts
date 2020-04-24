4986ed5742dd8c898aa33a52b6957461ced5db0d
nannalop <natalia17@ru.is>
2020-04-24 13:17:10 +0000

    Unfinished sentence in action interface for star in exercise

--- src/actions/exercisesActions/interface.ts
+++ src/actions/exercisesActions/interface.ts
@@ -2,21 +2,21 @@ import { ActionTypes } from "../types";
 import { Exercise } from "../../models";
 
 export interface GetExerciseSampleAction {
-	type: ActionTypes.getExerciseSample;
-	payload: Exercise[];
+    type: ActionTypes.getExerciseSample;
+    payload: Exercise[];
 }
 
 export interface GetExercisesByUserAction {
-	type: ActionTypes.getExercisesByUser;
-	payload: Exercise[];
+    type: ActionTypes.getExercisesByUser;
+    payload: Exercise[];
 }
 
 export interface ReloadSelectionAction {
-	type: ActionTypes.reloadSelection;
-	payload: null;
+    type: ActionTypes.reloadSelection;
+    payload: null;
 }
 
 export interface StarExerciseInSelectionAction {
-	type: Ac;
-	payload: { saved: boolean; exercise: string };
+    type: ActionTypes.starExerciseInSelection;
+    payload: { saved: boolean; exercise: string };
 }
