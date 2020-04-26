0ad55737e9fafd07be07099c120a206e70b2125c
njall skarphedinsson <njallis@gmail.com>
2020-04-26 16:15:21 +0000

    added ModifiedDot to Tab

--- src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/Tabs/Tab/index.tsx
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/Tabs/Tab/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Outer, CloseButton, Text } from "./styles";
+import { Outer, CloseButton, Text, ModifiedDot } from "./styles";
 import { IProps } from "./interface";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 
-export default ({ name, selected, onClick, onClose }: IProps) => {
+export default ({ name, selected, onClick, onClose, modified }: IProps) => {
 	return (
 		<Outer theme={{ selected }}>
 			<Text onClick={onClick}>{name ? name : "Ónefnt skjal"}</Text>
+			{modified ? <ModifiedDot /> : null}
 
 			<CloseButton onClick={onClose}>
 				<FontAwesomeIcon icon={faWindowClose} />
