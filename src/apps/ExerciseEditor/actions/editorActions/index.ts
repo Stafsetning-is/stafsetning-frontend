5e24579dd77bcf94e63b1359db436563b55898cb
njall skarphedinsson <njallis@gmail.com>
2020-04-26 09:50:35 +0000

    added SetCloseTabAction WriteToOpenFileAction

--- src/apps/ExerciseEditor/actions/editorActions/index.ts
+++ src/apps/ExerciseEditor/actions/editorActions/index.ts
@@ -20,4 +20,17 @@ export function createNewFile(): CreateNewFileAction {
 	};
 }
 
+export function setCloseTab(fileId: string): SetCloseTabAction {
+	return {
+		type: ActionTypes.setCloseTab,
+		payload: fileId,
+	};
+}
+
+export function writeToOpenFile(text: string): WriteToOpenFileAction {
+	return {
+		type: ActionTypes.writeToOpenFile,
+		payload: text,
+	};
+}
 export * from "./interface";
