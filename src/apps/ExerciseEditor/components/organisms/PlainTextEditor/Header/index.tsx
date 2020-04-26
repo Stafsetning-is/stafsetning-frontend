0aa3a46811bba9b6aade3df9c8dcd990b79c02a0
njall skarphedinsson <njallis@gmail.com>
2020-04-26 10:02:09 +0000

    Added header

---
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { EditorHeader } from "./styles";
+import Tabs from "./Tabs";
+
+export default () => {
+	return (
+		<EditorHeader>
+			<Tabs />
+		</EditorHeader>
+	);
+};
