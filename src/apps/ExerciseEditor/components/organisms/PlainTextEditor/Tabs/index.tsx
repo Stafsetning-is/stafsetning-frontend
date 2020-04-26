8556534bbba28668d67f5353d37047e0c5a3097a
njall skarphedinsson <njallis@gmail.com>
2020-04-26 09:52:13 +0000

    added tabs file

---
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Tabs/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { HeaderProps as IProps, BaseDocument } from "../interface";
+import Tab from "./Tab";
+import NewTab from "./NewTab";
+import { connect } from "react-redux";
+import { StoreState } from "../../../../reducers";
+import { setOpenTab, createNewFile, setCloseTab } from "../../../../actions";
+
+const Component = <T extends BaseDocument>({
+	documents,
+	openTab,
+	setOpenTab,
+	createNewFile,
+	setCloseTab
+}: IProps<T>) => {
+	return (
+		<React.Fragment>
+			{documents.map(({ fileName, _id }, i) => (
+				<Tab
+					name={fileName}
+					selected={_id === openTab}
+					onClick={() => setOpenTab(_id)}
+					onClose={() => setCloseTab(_id)}
+				/>
+			))}
+			<NewTab onClick={createNewFile} />
+		</React.Fragment>
+	);
+};
+
+const mapStateToProps = (state: StoreState) => ({
+	openTab: state.editor.openTab,
+	documents: state.editor.openFiles,
+});
+
+export default connect(mapStateToProps, { setOpenTab, createNewFile, setCloseTab })(
+	Component
+);
