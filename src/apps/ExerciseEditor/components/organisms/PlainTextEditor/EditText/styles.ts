0eef198438760a6a0a93a1cdfcdf938d7f0a2a78
njall skarphedinsson <njallis@gmail.com>
2020-04-26 10:30:09 +0000

    created styles

---
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/EditText/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const EditorFrame = styled.div`
+	padding: 20px;
+	background-color: #f8f8f8;
+	font-size: 18px;
+	height: 100%;
+	min-height: 400px;
+`;
