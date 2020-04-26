047c9ad515549c6ea7e3c12ac29e348b7182a3b3
njall skarphedinsson <njallis@gmail.com>
2020-04-26 16:12:20 +0000

    file grid added

---
+++ src/apps/ExerciseEditor/components/molecules/FileGrid/interface.ts
@@ -0,0 +1,5 @@
+import { File } from "../../../models";
+export interface IProps {
+	files: File[];
+	children: React.ReactNode;
+}
---
+++ src/apps/ExerciseEditor/components/molecules/FileGrid/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(6, 1fr);
+	grid-gap: 30px;
+`;
