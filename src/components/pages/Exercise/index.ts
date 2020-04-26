4dcb0f62640cb869bfab14084e5dda540e96951e
njall skarphedinsson <njallis@gmail.com>
2020-04-26 09:57:22 +0000

    refactoring

--- src/components/pages/Exercise/index.ts
+++ src/components/pages/Exercise/index.ts
@@ -1,11 +1,6 @@
-import Create from "./Create";
 import PracticeSession from "./PracticeSession";
 
 export default [
-	{
-		route: "/exercise/new/text-editor",
-		component: Create,
-	},
 	{
 		route: "/exercise/practice/:id/",
 		component: PracticeSession,
