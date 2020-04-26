90bd9a6d033cc7a343bce5130e28842694baa9e0
njall skarphedinsson <njallis@gmail.com>
2020-04-26 09:51:02 +0000

    added action types

--- src/apps/ExerciseEditor/actions/types/index.ts
+++ src/apps/ExerciseEditor/actions/types/index.ts
@@ -3,8 +3,12 @@ import * as EditorActions from "../editorActions";
 export enum ActionTypes {
 	setOpenTab,
 	createNewFile,
+	setCloseTab,
+	writeToOpenFile,
 }
 
 export type Actions =
 	| EditorActions.SetOpenTabAction
-	| EditorActions.CreateNewFileAction;
+	| EditorActions.CreateNewFileAction
+	| EditorActions.SetCloseTabAction
+	| EditorActions.WriteToOpenFileAction;
