7822125754fcbbff59319384d0d2e0a0117f87ad
njall skarphedinsson <njallis@gmail.com>
2020-04-26 09:53:12 +0000

    created Tab button in text editor

---
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Tabs/Tab/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Outer, CloseButton, Text } from "./styles";
+import { IProps } from "./interface";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
+
+export default ({ name, selected, onClick, onClose }: IProps) => {
+	return (
+		<Outer theme={{ selected }}>
+			<Text onClick={onClick}>
+				{name ? name : "Ónefnt skjal"}
+			</Text>
+			
+			<CloseButton onClick={onClose}>
+				<FontAwesomeIcon icon={faWindowClose} />
+			</CloseButton>
+		</Outer>
+	);
+};
