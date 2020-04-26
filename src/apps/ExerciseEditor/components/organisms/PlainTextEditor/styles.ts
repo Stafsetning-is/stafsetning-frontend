04f6d81fa38b670048aaee782c7aef33f20b4ed0
njall skarphedinsson <njallis@gmail.com>
2020-04-26 10:31:38 +0000

    refactoring

--- src/apps/ExerciseEditor/components/organisms/PlainTextEditor/styles.ts
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/styles.ts
@@ -1,13 +1,5 @@
 import styled from "styled-components";
 
-export const EditorFrame = styled.div`
-	padding: 20px;
-	background-color: #f8f8f8;
-	font-size: 18px;
-	height: 100%;
-	min-height: 400px;
-`;
-
 export const EditorOuter = styled.div`
 	border: 1px solid #343a40;
 	border-radius: 4px;
--- src/apps/ExerciseEditor/components/organisms/PlainTextEditor/utils.ts
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/utils.ts
@@ -1,13 +1,1 @@
-import { DocMap, BaseDocument } from "./interface";
-
-export const textToNodeArray = (text: string) => [
-	{
-		children: [{ text: text }],
-	},
-];
-
-export const handleKeyDown = (
-	event: React.KeyboardEvent<HTMLDivElement>
-): void => {
-	if (event.key == "Enter") event.preventDefault();
-};
+import { BaseDocument } from "./interface";
