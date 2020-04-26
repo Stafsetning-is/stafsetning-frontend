3d49531ffd56c3298a88ed60f0b3897a009f11a6
njall skarphedinsson <njallis@gmail.com>
2020-04-26 16:16:35 +0000

    Created file explorer page

---
+++ src/apps/ExerciseEditor/components/pages/FileExplorer/index.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { FileGrid, TextEditorIcon } from "../../";
+import { connect } from "react-redux";
+import { StoreState } from "../../../reducers";
+import { fetchFiles } from "../../../actions";
+import { IProps } from "./interface";
+
+const Component = ({ files, fetchFiles }: IProps) => {
+	useEffect(() => {
+		fetchFiles();
+	}, []);
+	return (
+		<div>
+			<FileGrid files={files}>
+				<TextEditorIcon fileName="Ritill" />
+			</FileGrid>
+		</div>
+	);
+};
+
+const mapStateToProps = (store: StoreState) => ({
+	files: store.desktop.files,
+});
+
+export default connect(mapStateToProps, { fetchFiles })(Component);
