a768fc066e64469f040a270750fe1b613ede762c
njall skarphedinsson <njallis@gmail.com>
2020-04-26 09:51:14 +0000

    created editor

---
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useMemo, useEffect } from "react";
+import { createEditor, Node } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+import { withHistory } from "slate-history";
+import { EditorFrame, EditorOuter, EditorHeader } from "./styles";
+import { textToNodeArray, mapDocArrayToDocMap, handleKeyDown } from "./utils";
+import Tabs from "./Tabs";
+import { TextEditorProps as IProps, BaseDocument, DocMap } from "./interface";
+import { connect } from "react-redux";
+import { StoreState } from "../../../reducers";
+import { writeToOpenFile } from "../../../actions";
+
+const Component = <Doc extends BaseDocument>({
+	documents,
+	selected,
+	writeToOpenFile,
+}: IProps<Doc>) => {
+	const openFile = documents.find((file) => file._id === selected);
+	console.log("openFile", openFile);
+	const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
+	const handleChange = (nodes: Node[]) => {
+		if (!(openFile && selected)) return;
+		const text = [...[...nodes].shift()?.children].shift().text;
+		writeToOpenFile(text);
+	};
+	// ATH refactora og buta niidur i buta.. hafa TD bara heeader, textframe, footer
+	// buta thetta MIKID BETUR NIDUR
+	return (
+		<EditorOuter>
+			<EditorHeader>
+				<Tabs />
+			</EditorHeader>
+			<EditorFrame>
+				{selected ? (
+					<Slate
+						editor={editor}
+						value={textToNodeArray(openFile ? openFile.text : "no open fle")}
+						onChange={handleChange}
+					>
+						<Editable
+							placeholder="Hér getur þú búið til æfingu"
+							onKeyDown={handleKeyDown}
+						/>
+					</Slate>
+				) : (
+					"no selected"
+				)}
+			</EditorFrame>
+		</EditorOuter>
+	);
+};
+
+const mapStateToProps = (state: StoreState) => ({
+	documents: state.editor.openFiles,
+	selected: state.editor.openTab,
+});
+
+export const PlainTextEditor = connect(mapStateToProps, { writeToOpenFile })(
+	Component
+);
