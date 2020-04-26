44c4771a66bbb7d52bee6af8a07c4196e6655eca
njall skarphedinsson <njallis@gmail.com>
2020-04-26 12:40:29 +0000

    added default name for unnamed files

--- src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/Tabs/Tab/index.tsx
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/Tabs/Tab/index.tsx
@@ -7,10 +7,8 @@ import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 export default ({ name, selected, onClick, onClose }: IProps) => {
 	return (
 		<Outer theme={{ selected }}>
-			<Text onClick={onClick}>
-				{name ? name : "Ónefnt skjal"}
-			</Text>
-			
+			<Text onClick={onClick}>{name ? name : "Ónefnt skjal"}</Text>
+
 			<CloseButton onClick={onClose}>
 				<FontAwesomeIcon icon={faWindowClose} />
 			</CloseButton>
