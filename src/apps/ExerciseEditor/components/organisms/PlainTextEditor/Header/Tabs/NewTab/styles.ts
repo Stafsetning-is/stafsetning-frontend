f4f0ec371469fd8c8bc15f9fb568e7f59e52b1c5
njall skarphedinsson <njallis@gmail.com>
2020-04-26 12:40:11 +0000

    refactoring

--- src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/Tabs/NewTab/styles.ts
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/Tabs/NewTab/styles.ts
@@ -1,19 +1,37 @@
 import styled from "styled-components";
 
-export const Outer = styled.div`
+export const Plus = styled.div`
 	position: inline-block;
 	height: 100%;
-	width: 30px;
+	width: 50px;
 	box-sizing: border-box;
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	color: #cecece;
-	cursor: pointer;
 	transition: 0.3s;
 	font-size: 15px;
 	:hover {
 		color: #f8f8f8;
-		margin-left: 2px;
+	}
+`;
+
+export const Info = styled.p`
+	height: 100%;
+	display: flex;
+	align-items: center;
+	color: #cecece;
+	margin-left: 20px;
+`;
+
+export const Outer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	cursor: pointer;
+	:hover,
+	:hover ${Info}, :hover ${Plus} {
+		color: #f8f8f8 !important;
 	}
 `;
---
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/Tabs/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Close = styled.div`
+	color: #ff0066;
+	height: 100%;
+	padding: 0 10px;
+	display: flex;
+	align-items: center;
+	font-size: 12px;
+	cursor: pointer;
+`;
+
+export const Maximize = styled.div`
+	color: rgba(255, 195, 55, 1);
+	height: 100%;
+	padding: 0 10px;
+	display: flex;
+	align-items: center;
+	font-size: 12px;
+	cursor: pointer;
+`;
+
+export const Collapse = styled.div`
+	color: #00ffaa;
+	height: 100%;
+	padding: 0 10px;
+	display: flex;
+	align-items: center;
+	font-size: 12px;
+	cursor: pointer;
+`;
