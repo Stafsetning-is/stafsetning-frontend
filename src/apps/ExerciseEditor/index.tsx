7ad2436400ca305e544890ed2ecb2cb5b489dee3
njall skarphedinsson <njallis@gmail.com>
2020-04-26 16:17:25 +0000

    added text editor to SubApp's root

--- src/apps/ExerciseEditor/index.tsx
+++ src/apps/ExerciseEditor/index.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Routing from "./routing";
 import { Provider } from "react-redux";
 import store from "./store";
+import { PlainTextEditor } from "./components";
 
 const App = () => {
 	return (
 		<Provider store={store}>
+			<PlainTextEditor />
 			<Routing />
 		</Provider>
 	);
