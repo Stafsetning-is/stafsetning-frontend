79ddadbebcba0fe3bfd64dc71a831075663e8bc9
njall skarphedinsson <njallis@gmail.com>
2020-04-27 13:37:31 +0000

    added error message

--- src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/index.tsx
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { EditorHeader } from "./styles";
 import Tabs from "./Tabs";
+import ErrorMessage from "./ErrorMessage";
 
 export default () => {
 	return (
 		<EditorHeader>
+			<ErrorMessage />
 			<Tabs />
 		</EditorHeader>
 	);
