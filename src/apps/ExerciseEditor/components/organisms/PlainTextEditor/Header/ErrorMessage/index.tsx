3ba38a121accc4cc45fd61d32d55bca89ba247ce
njall skarphedinsson <njallis@gmail.com>
2020-04-27 13:37:15 +0000

    created error message

---
+++ src/apps/ExerciseEditor/components/organisms/PlainTextEditor/Header/ErrorMessage/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Outer } from "./styles";
+import { IProps } from "./interface";
+import { connect } from "react-redux";
+import { StoreState } from "../../../../../reducers";
+import { clearErrorMessage } from "../../../../../actions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+const Component = ({ message, clearErrorMessage }: IProps) => {
+	const hasMessage = !!message;
+	return (
+		<Outer theme={{ hasMessage }}>
+			<span>{message}</span>
+			<FontAwesomeIcon icon={faTimes} onClick={clearErrorMessage} />
+		</Outer>
+	);
+};
+
+const mapStateToProps = (state: StoreState) => ({
+	message: state.editor.errorMessage,
+});
+
+export default connect(mapStateToProps, { clearErrorMessage })(Component);
