43a9aae12edd8fff60f675bbba1c4afcf3ba1cf3
njall skarphedinsson <njallis@gmail.com>
2020-04-27 13:35:09 +0000

    added properties to FileIcoon child

--- src/apps/ExerciseEditor/components/molecules/FileGrid/index.tsx
+++ src/apps/ExerciseEditor/components/molecules/FileGrid/index.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { FileIcon } from "../../";
 import { IProps } from "./interface";
-import { Grid } from "./styles";
+import { Grid, Outer } from "./styles";
+import { connect } from "react-redux";
+import { StoreState } from "../../../reducers";
+const Component = ({ files, children, selected }: IProps) => {
+	const selectedDict: { [key: string]: boolean } = {};
+	selected.forEach((key) => (selectedDict[key] = true));
 
-export const FileGrid = ({ files, children }: IProps) => {
 	return (
-		<Grid>
-			{children}
-			{files.map((file) => (
-				<FileIcon fileName={file.fileName} file={file} />
-			))}
-		</Grid>
+		<Outer>
+			<Grid>
+				{children}
+				{files.map((file) => (
+					<FileIcon
+						fileName={file.fileName}
+						file={file}
+						selected={selectedDict[file._id] ?? false}
+						_id={file._id}
+					/>
+				))}
+			</Grid>
+		</Outer>
 	);
 };
+
+const mapStateToProps = (state: StoreState) => ({
+	selected: state.desktop.selected,
+});
+
+export const FileGrid = connect(mapStateToProps)(Component);
