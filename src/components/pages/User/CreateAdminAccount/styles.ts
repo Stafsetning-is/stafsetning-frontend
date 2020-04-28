0c3fce183e91a1d5d3b6fbdef0d68a1bf19cf752
njall skarphedinsson <njallis@gmail.com>
2020-04-28 15:14:40 +0000

    added styling

---
+++ src/components/pages/User/CreateAdminAccount/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Outer = styled.div`
+	margin: 50px auto;
+	width: 500px;
+`;
+
+export const InfoBox = styled.div`
+	width: 100%;
+	border: 1px dashed #cecece;
+	border-radius: 7px;
+	padding: 20px;
+	box-sizing: border-box;
+	color: #666;
+	line-height: 1.45;
+`;
