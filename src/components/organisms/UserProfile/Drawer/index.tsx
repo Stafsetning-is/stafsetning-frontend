fb16c45acb24a47812bf0f3c6d63113dee6d4649
gabriels17 <gabriel.sighvatsson@gmail.com>
2020-04-28 16:39:23 +0000

    Uses Drawer list

--- src/components/organisms/UserProfile/Drawer/index.tsx
+++ src/components/organisms/UserProfile/Drawer/index.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { MouseEvent } from "react";
 import { Frame, Button } from "./styles";
+import { Drawers } from "./interface";
+import { DRAWER_LIST } from "./utils";
 /* Should contain a line and under the line we have a two clickable options */
 
 export default () => {
+    const handleClick = (toOpen: Drawers) => {};
+    //const button =  ? kláraðar : vistaðar
     return (
         <Frame>
-            <Button>Kláraðar æfingar</Button>
-            <Button>Vistaðar æfingar</Button>
+            {DRAWER_LIST.map((item) => (
+                <Button onClick={() => handleClick(item.drawerType)}>
+                    {item.label}
+                </Button>
+            ))}
         </Frame>
     );
 };
