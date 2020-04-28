a0e2f189d4b4a9cffbb09453e72e2b0d6257e2d5
njall skarphedinsson <njallis@gmail.com>
2020-04-28 15:14:51 +0000

    added route

--- src/components/pages/User/index.ts
+++ src/components/pages/User/index.ts
@@ -1,13 +1,18 @@
 import Profile from "./Profile";
 import Settings from "./Settings";
+import CreateAdminAccount from "./CreateAdminAccount";
 
 export default [
-    {
-        route: "/user/profile",
-        component: Profile,
-    },
-    {
-        route: "/user/settings",
-        component: Settings,
-    },
+	{
+		route: "/user/profile",
+		component: Profile,
+	},
+	{
+		route: "/user/settings",
+		component: Settings,
+	},
+	{
+		route: "/user/create-admin-account/",
+		component: CreateAdminAccount,
+	},
 ];
