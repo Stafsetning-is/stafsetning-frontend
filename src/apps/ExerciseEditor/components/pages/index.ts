6e5110f8623df806c5b59425306d8228ba4472c0
njall skarphedinsson <njallis@gmail.com>
2020-04-28 13:56:04 +0000

    added PublishSuccess to routing

--- src/apps/ExerciseEditor/components/pages/index.ts
+++ src/apps/ExerciseEditor/components/pages/index.ts
@@ -1,7 +1,12 @@
 import FileExplorer from "./FileExplorer";
 import PublisherPage from "./PublisherPage";
+import PublishSuccess from "./PublishSuccess";
 
 export default [
+	{
+		route: "*/publish/done/",
+		component: PublishSuccess,
+	},
 	{
 		route: "*/publish/:id",
 		component: PublisherPage,
