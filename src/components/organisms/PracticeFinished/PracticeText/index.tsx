0adcb99731e228420148cc4e6517432f27e851f9
njall skarphedinsson <njallis@gmail.com>
2020-04-28 13:58:22 +0000

    cwt

--- src/components/organisms/PracticeFinished/PracticeText/index.tsx
+++ src/components/organisms/PracticeFinished/PracticeText/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { IProps } from "./interface";
 import Char from "../Character";
-import { Practice } from "./interface";
 import { Outer } from "./styles";
 
 /**
@@ -9,28 +8,28 @@ import { Outer } from "./styles";
  */
 
 export default ({ errors, exerciseString }: IProps) => {
-    const charArray = exerciseString.split("");
+	const charArray = exerciseString.split("");
 
-    /**
-     * GetErrorAtIndex function takes an index
-     * It returns error if its an error and returns "" (empty string) if it is not and error
-     */
+	/**
+	 * GetErrorAtIndex function takes an index
+	 * It returns error if its an error and returns "" (empty string) if it is not and error
+	 */
 
-    const getIndexOfError = (index: number) => {
-        const errorItem = errors.find((error) => error.charAt === index);
-        if (errorItem === undefined) return "";
-        return errorItem.error;
-    };
+	const getIndexOfError = (index: number) => {
+		const errorItem = errors.find((error) => error.charAt === index);
+		if (errorItem === undefined) return "";
+		return errorItem.error;
+	};
 
-    /**
-     * characterJSX essentially calls getErrorAtIndex and sets itself as that return value along with the index
-     */
-    const characterJSX = charArray.map((char, index) => {
-        const errorChar = getIndexOfError(index);
-        return <Char char={char} error={errorChar} />;
-    });
-    /**
-     * Here we return the characterJSX
-     */
-    return <Outer>{characterJSX} </Outer>;
+	/**
+	 * characterJSX essentially calls getErrorAtIndex and sets itself as that return value along with the index
+	 */
+	const characterJSX = charArray.map((char, index) => {
+		const errorChar = getIndexOfError(index);
+		return <Char char={char} error={errorChar} />;
+	});
+	/**
+	 * Here we return the characterJSX
+	 */
+	return <Outer>{characterJSX} </Outer>;
 };
