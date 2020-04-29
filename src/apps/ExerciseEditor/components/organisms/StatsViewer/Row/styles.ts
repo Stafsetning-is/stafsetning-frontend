421a1383ae421b51bc1bd0b675a8ea5446781a85
njall skarphedinsson <njallis@gmail.com>
2020-04-29 10:52:44 +0000

    created stats viewer organism

---
+++ src/apps/ExerciseEditor/components/organisms/StatsViewer/Row/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Inner = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+export const Name = styled.div`
+	color: #666;
+	font-weight: 500;
+	font-size: 16px;
+`;
+export const ViewCount = styled.div`
+	color: #666;
+	display: flex;
+	margin-left: 10px;
+	margin-top: 5px;
+	margin-right: 3px;
+`;
+
+export const IconContainer = styled.div`
+	color: #6600ff;
+	margin: 0 5px;
+`;
+
+export const RightContainer = styled.div`
+	display: flex;
+`;
---
+++ src/apps/ExerciseEditor/components/organisms/StatsViewer/utils.ts
@@ -0,0 +1,3 @@
+export const TITLE = "Þú hefur ekki enn búið til æfingu.";
+export const TEXT =
+	"Smelltu á Ritilinn til þess að búa til æfingu. Þegar þú hefur sent hana inn þá geturðu séð tölfræðina hér!";
