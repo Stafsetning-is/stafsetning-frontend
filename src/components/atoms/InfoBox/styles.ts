ce1fa794163d326d87e68bb85ff5f36dff60d1a9
njall skarphedinsson <njallis@gmail.com>
2020-04-29 10:54:42 +0000

    added generic info box

---
+++ src/components/atoms/InfoBox/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Outer = styled.div`
+	width: 100%;
+	border: 1px dashed #cecece;
+	border-radius: 7px;
+	padding: 20px;
+	box-sizing: border-box;
+	color: #666;
+	line-height: 1.45;
+`;
