abf6697e30d8b8800bee1a9ae3a7ded6f573992b
Júlía <juliai17@ru.is>
2020-04-30 21:53:35 +0000

    added fetchtrophies to reducer

--- src/reducers/userProfileReducer/index.ts
+++ src/reducers/userProfileReducer/index.ts
@@ -2,34 +2,40 @@ import { UserProfileState } from "./interface";
 import { ActionTypes, Actions } from "../../actions/types";
 
 export const initialState: UserProfileState = {
-    saved: [],
-    finished: [],
-    openDrawer: "finished"
+	saved: [],
+	finished: [],
+	trophies: [],
+	openDrawer: "finished",
 };
 
 export default (
-    state: UserProfileState = initialState,
-    action: Actions
+	state: UserProfileState = initialState,
+	action: Actions
 ): UserProfileState => {
-    switch (action.type) {
-        case ActionTypes.getFinishedExercises:
-            return {
-                ...state,
-                finished: action.payload,
-            };
-        case ActionTypes.getSavedExercises:
-            return {
-                ...state,
-                saved: action.payload,
-            }
-        case ActionTypes.setOpenDrawer:
-            return {
-                ...state,
-                openDrawer: action.payload
-            }
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case ActionTypes.getFinishedExercises:
+			return {
+				...state,
+				finished: action.payload,
+			};
+		case ActionTypes.getSavedExercises:
+			return {
+				...state,
+				saved: action.payload,
+			};
+		case ActionTypes.setOpenDrawer:
+			return {
+				...state,
+				openDrawer: action.payload,
+			};
+		case ActionTypes.fetchTrophies:
+			return {
+				...state,
+				trophies: action.payload,
+			};
+		default:
+			return state;
+	}
 };
 
 export * from "./interface";
