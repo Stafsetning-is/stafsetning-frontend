b33e937dea5ae830084cf409a3a6db1d673cf2d4
nannalop <natalia17@ru.is>
2020-04-30 17:52:36 +0000

    types for new action

--- src/actions/types/index.ts
+++ src/actions/types/index.ts
@@ -5,52 +5,54 @@ import * as UserProfileActions from "../userProfileActions";
 import * as SocketActions from "../socketActions";
 
 export enum ActionTypes {
-	setMinWordCount,
-	setHideCompleted,
-	setMaxWordCount,
-	logInUser,
-	logOutUser,
-	getExerciseSample,
-	getExercisesByUser,
-	getFinishedExercises,
-	addGrammarRuleToFilter,
-	removeGrammarRuleFromFilter,
-	setQuickFilter,
-	closeFilterSideBar,
-	openFilterSideBar,
-	reloadSelection,
-	starExerciseInSelection,
-	fetchSavedExercises,
-	fetchAdminInvites,
-	requestAdminStatusForUser,
-	changePendingAdminInviteToLoading,
-	setActiveUsers,
-	getSavedExercises,
-	setOpenDrawer,
-	changeUserPoints,
+    setMinWordCount,
+    setHideCompleted,
+    setMaxWordCount,
+    logInUser,
+    logOutUser,
+    getExerciseSample,
+    getExercisesByUser,
+    getFinishedExercises,
+    addGrammarRuleToFilter,
+    removeGrammarRuleFromFilter,
+    setQuickFilter,
+    closeFilterSideBar,
+    openFilterSideBar,
+    reloadSelection,
+    starExerciseInSelection,
+    fetchSavedExercises,
+    fetchAdminInvites,
+    requestAdminStatusForUser,
+    changePendingAdminInviteToLoading,
+    setActiveUsers,
+    getSavedExercises,
+    setOpenDrawer,
+    changeUserPoints,
+    changeDifficulty
 }
 
 export type Actions =
-	| FilterActions.SetMinWordCountAction
-	| FilterActions.SetHideCompletedAction
-	| FilterActions.SetMaxWordCountAction
-	| AuthActions.LogInUserAction
-	| AuthActions.LogOutUserAction
-	| ExercisesActions.GetExerciseSampleAction
-	| ExercisesActions.GetExercisesByUserAction
-	| UserProfileActions.GetFinishedExercisesAction
-	| FilterActions.AddRuleToGrammarFilterAction
-	| FilterActions.RemoveGrammarRuleFromFilterAction
-	| FilterActions.SetQuickFilterAction
-	| FilterActions.CloseFilterAction
-	| FilterActions.OpenFilterAction
-	| ExercisesActions.ReloadSelectionAction
-	| ExercisesActions.StarExerciseInSelectionAction
-	| AuthActions.FetchAdminInvitesAction
-	| AuthActions.RequestAdminStatusForUserAction
-	| AuthActions.ChangePendingAdminInviteToLoadingAction
-	| UserProfileActions.GetSavedExercisesAction
-	| UserProfileActions.SetOpenDrawerAction
-	| AuthActions.ChangePendingAdminInviteToLoadingAction
-	| SocketActions.SetActiveUsersAction
-	| AuthActions.ChangeUserPointsAction;
+    | FilterActions.SetMinWordCountAction
+    | FilterActions.SetHideCompletedAction
+    | FilterActions.SetMaxWordCountAction
+    | AuthActions.LogInUserAction
+    | AuthActions.LogOutUserAction
+    | ExercisesActions.GetExerciseSampleAction
+    | ExercisesActions.GetExercisesByUserAction
+    | UserProfileActions.GetFinishedExercisesAction
+    | FilterActions.AddRuleToGrammarFilterAction
+    | FilterActions.RemoveGrammarRuleFromFilterAction
+    | FilterActions.SetQuickFilterAction
+    | FilterActions.CloseFilterAction
+    | FilterActions.OpenFilterAction
+    | ExercisesActions.ReloadSelectionAction
+    | ExercisesActions.StarExerciseInSelectionAction
+    | AuthActions.FetchAdminInvitesAction
+    | AuthActions.RequestAdminStatusForUserAction
+    | AuthActions.ChangePendingAdminInviteToLoadingAction
+    | UserProfileActions.GetSavedExercisesAction
+    | UserProfileActions.SetOpenDrawerAction
+    | AuthActions.ChangePendingAdminInviteToLoadingAction
+    | SocketActions.SetActiveUsersAction
+    | AuthActions.ChangeUserPointsAction
+    | UserProfileActions.ChangeDifficultyAction;
