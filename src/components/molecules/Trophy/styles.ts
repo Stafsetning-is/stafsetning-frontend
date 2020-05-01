4e52f337ab2ca2abf27e2166dd4839b54330e838
Júlía <juliai17@ru.is>
2020-05-01 13:22:49 +0000

    styles for trophy

--- src/components/molecules/Trophy/styles.ts
+++ src/components/molecules/Trophy/styles.ts
@@ -1,9 +1,33 @@
 import styled from "styled-components";
 
-export const Outer = styled.div``;
+const SIZE = 110;
 
-export const Title = styled.div``;
+export const Outer = styled.div`
+	border: 1px dotted rgba(199, 135, 8, 0.6);
+	border-radius: ${SIZE / 2}px;
+	width: ${SIZE}px;
+	height: ${SIZE}px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	background-color: rgba(199, 135, 8, 0.12);
+	position: relative;
+	box-shadow: 0px 0px 15px rgba(102, 0, 255, 0.1);
+	transition: 0.55s;
+	:hover {
+		box-shadow: 0px 0px 25px rgba(102, 0, 255, 0.15);
+	}
+`;
 
-export const Description = styled.div``;
+export const Title = styled.div`
+	color: #333;
+	font-weight: 200;
+`;
 
-export const Icon = styled.div``;
+export const Icon = styled.div`
+	color: #e8bd00;
+	opacity: 0.6;
+	font-size: 40px;
+	margin-bottom: 8px;
+`;
