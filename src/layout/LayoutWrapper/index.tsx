16fef6a4a57451b3caa65fa50c9901bccfe8e873
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-01 12:32:48 +0000

    cwt

--- src/layout/LayoutWrapper/index.tsx
+++ src/layout/LayoutWrapper/index.tsx
@@ -15,13 +15,13 @@ import { connect } from "react-redux";
 import { SIGNED_IN_USER_LEVELS, GUEST, ADMIN } from "./utils";
 
 const Component = ({
-	children,
-	userType,
-	fetchExercisesForUser,
-	fetchExercisesSample,
-	fetchUserFromToken,
-	fetchAdminInviteList,
-	userId,
+    children,
+    userType,
+    fetchExercisesForUser,
+    fetchExercisesSample,
+    fetchUserFromToken,
+    fetchAdminInviteList,
+    userId,
 }: IProps) => {
     /**
      * Fetches info about logged in
@@ -31,22 +31,23 @@ const Component = ({
         fetchUserFromToken();
     }, [fetchUserFromToken]);
 
-	/**
-	 * Fetches exercises for front page
-	 * when ever the user type changes
-	 */
-	useEffect(() => {
-		if (userType === GUEST) fetchExercisesSample();
-		else if (SIGNED_IN_USER_LEVELS.includes(userType)) fetchExercisesForUser();
-	}, [userType, fetchExercisesSample, fetchExercisesForUser]);
+    /**
+     * Fetches exercises for front page
+     * when ever the user type changes
+     */
+    useEffect(() => {
+        if (userType === GUEST) fetchExercisesSample();
+        else if (SIGNED_IN_USER_LEVELS.includes(userType))
+            fetchExercisesForUser();
+    }, [userType, fetchExercisesSample, fetchExercisesForUser]);
 
-	/**
-	 * fetches list of pending users
-	 * for admin priveledges
-	 */
-	useEffect(() => {
-		if (userType === ADMIN) fetchAdminInviteList();
-	}, [userType]);
+    /**
+     * fetches list of pending users
+     * for admin priveledges
+     */
+    useEffect(() => {
+        if (userType === ADMIN) fetchAdminInviteList();
+    }, [userType]);
 
     return (
         <BackDrop>
@@ -61,12 +62,8 @@ const Component = ({
 };
 
 const mapStateToProps = (state: StoreState) => ({
-<<<<<<< HEAD
     userType: state.auth.type,
-=======
-	userType: state.auth.type,
-	userId: state.auth.user._id,
->>>>>>> 3a2ccdbdfb9aa351a86b4eb7d91c0155a4b2d276
+    userId: state.auth.user._id,
 });
 
 export const LayoutWrapper = connect(mapStateToProps, {
