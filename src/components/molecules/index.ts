132a7ece10da091b40b378bee0a4eaef631f5a1e
Júlía <juliai17@ru.is>
2020-05-01 13:21:23 +0000

    export grid and loadinggrid

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -3,4 +3,6 @@ export * from "./LoaderBox";
 export * from "./InputFactory";
 export * from "./ExerciseBox";
 export * from "./TrophyModal";
+export * from "./Grid";
+export * from "./LoadingGrid";
 export * from "./Trophy";
