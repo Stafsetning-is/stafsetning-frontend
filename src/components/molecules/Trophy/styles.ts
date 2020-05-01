93514a518621f3fe051bdf0545429569ac9f91e1
Júlía <juliai17@ru.is>
2020-05-01 12:08:17 +0000

    created styles for trophy

---
+++ src/components/molecules/Trophy/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Outer = styled.div``;
+
+export const Title = styled.div``;
+
+export const Description = styled.div``;
+
+export const Icon = styled.div``;
