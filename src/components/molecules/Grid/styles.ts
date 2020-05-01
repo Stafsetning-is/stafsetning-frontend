5a2ed75313cec41db0520cd75e1ce6fd2b0066fe
Júlía <juliai17@ru.is>
2020-05-01 13:20:45 +0000

    added itemsinarow property

--- src/components/molecules/Grid/styles.ts
+++ src/components/molecules/Grid/styles.ts
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 	display: grid;
-	grid-template-columns: repeat(3, 1fr);
+	grid-template-columns: repeat(${(props) => props.theme.itemsInRow}, 1fr);
 	grid-gap: 30px;
 `;
