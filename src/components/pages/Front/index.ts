dc85c8968d2ed2cf1c8631d1c666a64da894674f
gabriels17 <gabriel.sighvatsson@gmail.com>
2020-05-09 22:49:26 +0000

    Added a route to the cookie page

--- src/components/pages/Front/index.ts
+++ src/components/pages/Front/index.ts
@@ -1,8 +1,13 @@
 import LandingPage from "./LandingPage";
+import CookieInfoPage from "./CookieInfoPage";
 
 export default [
-	{
-		route: "/",
-		component: LandingPage
-	}
+    {
+        route: "/",
+        component: LandingPage
+    },
+    {
+        route: "/cookies",
+        component: CookieInfoPage
+    }
 ];
