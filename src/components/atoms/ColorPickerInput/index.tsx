3c5098688ef2bc2358882a5e2b4a9899671d5e55
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 14:40:12 +0000

    added comments for colorPickerInput

--- src/components/atoms/ColorPickerInput/index.tsx
+++ src/components/atoms/ColorPickerInput/index.tsx
@@ -3,61 +3,68 @@ import { FormLabel } from "..";
 import { Grid } from "../../";
 import { IProps } from "./interface";
 import {
-	Outer,
-	PickedColor,
-	ColorBox,
-	Selections,
-	Text,
-	Inner,
+    Outer,
+    PickedColor,
+    ColorBox,
+    Selections,
+    Text,
+    Inner,
 } from "./styles";
+
 export const ColorPickerInput = ({
-	value,
-	onChange,
-	label,
-	validationMessage,
-	colors,
+    value,
+    onChange,
+    label,
+    validationMessage,
+    colors,
 }: IProps) => {
-	const [isOpen, setOpen] = useState(false);
+    const [isOpen, setOpen] = useState(false);
 
-	// opens or keeps open
-	const open = () => setOpen(true);
-	// closes or keeps closed
-	const close = () => setOpen(false);
+    // opens or keeps open
+    const open = () => setOpen(true);
+    // closes or keeps closed
+    const close = () => setOpen(false);
 
-	/**
-	 * Returns validation message
-	 */
-	const getValidationMessage = () => {
-		if (!value) return "";
-		return validationMessage ?? "";
-	};
-
-	const handleSelection = (value: any) => {
-		onChange(value);
-		close();
-	};
-
-	return (
-		<Outer onMouseLeave={close}>
-			<FormLabel text={label} errorMessage={getValidationMessage()} />
-			<Inner onClick={open}>
-				<PickedColor>
-					<ColorBox theme={{ color: value }} />
-				</PickedColor>
-				<Text>Veldu lit</Text>
-				{isOpen ? (
-					<Selections>
-						<Grid itemsInRow={4} isEmpty={false}>
-							{colors.map((color) => (
-								<ColorBox
-									theme={{ color }}
-									onClick={() => handleSelection(color)}
-								/>
-							))}
-						</Grid>
-					</Selections>
-				) : null}
-			</Inner>
-		</Outer>
-	);
+    /**
+     * Returns validation message
+     */
+    const getValidationMessage = () => {
+        if (!value) return "";
+        return validationMessage ?? "";
+    };
+    /**
+     * when the value changes, the colorPicker closes
+     */
+    const handleSelection = (value: any) => {
+        onChange(value);
+        close();
+    };
+    /**
+     * Here the color picker opens up when the user clicks on it
+     * It closes when the mouse moves away from the form
+     * when the user clicks on a color, handleSelection will change the currently selected color regarding the background color in the exercise box
+     */
+    return (
+        <Outer onMouseLeave={close}>
+            <FormLabel text={label} errorMessage={getValidationMessage()} />
+            <Inner onClick={open}>
+                <PickedColor>
+                    <ColorBox theme={{ color: value }} />
+                </PickedColor>
+                <Text>Veldu lit</Text>
+                {isOpen ? (
+                    <Selections>
+                        <Grid itemsInRow={4} isEmpty={false}>
+                            {colors.map((color) => (
+                                <ColorBox
+                                    theme={{ color }}
+                                    onClick={() => handleSelection(color)}
+                                />
+                            ))}
+                        </Grid>
+                    </Selections>
+                ) : null}
+            </Inner>
+        </Outer>
+    );
 };
