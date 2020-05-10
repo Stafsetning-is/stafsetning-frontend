fef4320fd8b2ba7eff8d160f78148bfd7278d45d
njall skarphedinsson <njallis@gmail.com>
2020-05-10 13:11:11 +0000

    added comments to actions folder

--- src/actions/authActions/index.ts
+++ src/actions/authActions/index.ts
@@ -17,11 +17,41 @@ import { emitLogin } from "../";
 import { removeToken } from "../../services";
 
 /**
- * logs the user in and emits the login
- * @param user
+ * AUTH ACTIONS
+ *
+ * Here are all actions that change or maniuplate
+ * the auth state in the auth reducer. These actions
+ * mainly deal with the authentication part
+ * of the application as well as the state
+ * of the currently logged in user, or other
+ * users that are stored for various purposes
+ *
+ * example of other user data are
+ *     - list of users pending admin approval
+ *
+ * These actions are
+ *     - logInUser
+ *     - fetchUserFromToken
+ *     - changePendingAdminInviteToLoading
+ *     - requestAdminStatusForUser
+ *     - fetchAdminInviteList
+ *     - signOut
+ *     - changeUserPoints
+ *     - changeUserPreferences
+ *     - changeUserDifficulty
+ *     - changeUserGender
+ *     - openPickGenderView
+ *
+ */
+
+/**
+ * logs the user
+ * @param user conforms to user interface
  */
 export function logInUser(user: User): LogInUserAction {
+	// emits log in via socket
 	emitLogin(user._id);
+	// returns LogInUserAction
 	return {
 		type: ActionTypes.logInUser,
 		payload: user,
@@ -29,16 +59,23 @@ export function logInUser(user: User): LogInUserAction {
 }
 
 /**
- * Fetched the user info from token
- * this should be used on application
- * start
+ * fetches the user from the api
+ * by checking the token in cookies
+ * if the token is not valid
+ * then we dispatch a LogOutUserAction
+ * else we log him in
  */
 export function fetchUserFromToken() {
 	return async function (dispatch: Dispatch) {
 		try {
+			// we call the /users/auth route
+			// Api handles the cookies
+			// throws 401 if no session
 			const { data } = await Api.get<User>("/api/v1/users/auth");
 			dispatch<LogInUserAction>(logInUser(data));
 		} catch (error) {
+			// if 401 is thrown then user
+			// should not be logged in
 			dispatch<LogOutUserAction>({
 				type: ActionTypes.logOutUser,
 				payload: null,
@@ -49,12 +86,13 @@ export function fetchUserFromToken() {
 
 /**
  * changes an user with "admin-invite-pending" to "loading"
- * while the request has not responded
+ * while the request has not given an answer
  * @param id users id
  */
 export function changePendingAdminInviteToLoading(
 	id: string
 ): ChangePendingAdminInviteToLoadingAction {
+	// returns the action
 	return {
 		type: ActionTypes.changePendingAdminInviteToLoading,
 		payload: id,
@@ -62,82 +100,121 @@ export function changePendingAdminInviteToLoading(
 }
 
 /**
- * Request admin status for user
+ * An async action creater that requests admin
+ * status for another user.
+ * @param id the user to make admin
  */
 export function requestAdminStatusForUser(id: string) {
+	// return async action
 	return async function (dispatch: Dispatch) {
 		try {
+			// start by changing the user status to loading
 			dispatch<ChangePendingAdminInviteToLoadingAction>({
 				type: ActionTypes.changePendingAdminInviteToLoading,
 				payload: id,
 			});
+			// send the request
 			await Api.post<void>(`/api/admin/users/${id}/make_admin`);
+			// if it works then we dispatch to redux
 			dispatch<RequestAdminStatusForUserAction>({
 				type: ActionTypes.requestAdminStatusForUser,
 				payload: id,
 			});
 		} catch (error) {
-			// error
+			// do nothing on error
 		}
 	};
 }
 
 /**
- * fetches pending admin invites from backend
+ * Async action creater that fetches
+ * the invite list for users requesting
+ * admin priveledges
  */
-
 export function fetchAdminInviteList() {
+	// returns async action creator
 	return async function (dispatch: Dispatch) {
 		try {
+			// calls the route
 			const { data } = await Api.get<User[]>(
 				"/api/admin/users/invite_list/"
 			);
+			// dispatch .fetchAdminInvites if route works
 			dispatch<FetchAdminInvitesAction>({
 				type: ActionTypes.fetchAdminInvites,
 				payload: data,
 			});
 		} catch (error) {
-			// error during fetching
+			// do nothing if error
 		}
 	};
 }
 
+/**
+ * sync action creator that logs user
+ * out
+ */
 export function signOut(): LogOutUserAction {
+	// removes the token
 	removeToken();
+	// returns the action creator
 	return {
 		type: ActionTypes.logOutUser,
 		payload: null,
 	};
 }
 
+/**
+ * updates the points of the user that is
+ * logged in
+ * @param points new point value
+ */
 export function changeUserPoints(points: number): ChangeUserPointsAction {
+	// returns the action creator
 	return {
 		type: ActionTypes.changeUserPoints,
 		payload: points,
 	};
 }
 
+/**
+ * updates user preferences in the store
+ * @param preferences UserPreference interface object
+ */
 export function changeUserPreferences(
 	preferences: UserPreference
 ): ChangeUserPreferencesAction {
+	// returns the action creator
 	return {
 		type: ActionTypes.changeUserPreferences,
 		payload: preferences,
 	};
 }
 
+/**
+ * changes the user difficulty in the store
+ * @param value new number between 1-11
+ */
 export function changeUserDifficulty(
 	value: number
 ): ChangeUserDifficulltyAction {
+	// returns the action
 	return {
 		type: ActionTypes.changeUserDifficulty,
 		payload: value,
 	};
 }
 
+/**
+ * async action creator that changes user gender
+ * in the api and updates store
+ * @param gender must be 'male' or 'female'
+ */
 export function changeUserGender(gender: "male" | "female") {
+	// returns async action creator
 	return async function (dispatch: Dispatch) {
 		try {
+			// sets gender type to 'loading'
 			dispatch<SetGenderAction>({
 				payload: "loading",
 				type: ActionTypes.setGender,
@@ -155,7 +232,11 @@ export function changeUserGender(gender: "male" | "female") {
 	};
 }
 
+/**
+ * Sets open gender view as open
+ */
 export function openPickGenderView(): SetGenderAction {
+	// returns the action
 	return {
 		type: ActionTypes.setGender,
 	};
--- src/actions/exercisesActions/index.ts
+++ src/actions/exercisesActions/index.ts
@@ -8,42 +8,72 @@ import {
 } from "./interface";
 import { ActionTypes } from "../types";
 
+/**
+ * EXERCISE ACTIONS
+ *
+ * Here are all the actions that
+ * deal with the list of exercises that are
+ * stored in the exercisesReducer.
+ *
+ * These actions are:
+ *     - fetchExerciseSample
+ *     - fetchExercisesForUser
+ *     - reloadSelection
+ *     - starExerciseInSelection
+ */
+
+/**
+ * Returns async action creator that fetches
+ * an sample of exercise for an guest
+ * that is not logged in
+ */
 export function fetchExercisesSample() {
+	// returns async action creator
 	return async function (dispatch: Dispatch) {
 		try {
+			// fetches from open api
 			const { data } = await Api.get<Exercise[]>(
 				"/api/open/exercise_sample"
 			);
 
+			// dispatches the action
 			dispatch<GetExerciseSampleAction>({
 				type: ActionTypes.getExerciseSample,
 				payload: data,
 			});
 		} catch (error) {
-			console.log("error", error);
-			console.log("Error getting sample");
+			// on error we do nothing
 		}
 	};
 }
 
+/**
+ * returns async action creator that
+ * fetches exercises for certain user
+ */
 export function fetchExercisesForUser() {
+	// returns async action creator
 	return async function (dispatch: Dispatch) {
 		try {
+			// calls the api to get the exercises array
 			const { data } = await Api.get<Exercise[]>(
 				"/api/v1/exercises/by_difficulty"
 			);
 
+			// dispatches the action
 			dispatch<GetExercisesByUserAction>({
 				type: ActionTypes.getExercisesByUser,
 				payload: data,
 			});
 		} catch (error) {
-			console.log("error", error);
-			console.log("Error getting exercises");
+			// on error we do nothing
 		}
 	};
 }
 
+/**
+ * action creator that reloads the selection
+ */
 export function reloadSelection() {
 	return {
 		type: ActionTypes.reloadSelection,
@@ -51,19 +81,28 @@ export function reloadSelection() {
 	};
 }
 
+/**
+ * Save an exercise in selection
+ * @param id exercise id
+ * @param save should it save (true) or unsave (false)
+ */
 export function starExerciseInSelection(id: string, save: boolean) {
+	// concat endpoint based on wether save is true or false
 	const saveEndpoint =
 		`/api/v1/users/exercises/${id}/` + (save ? "save" : "unsave");
 
+	// returns acyn action creator
 	return async function (dispatch: Dispatch) {
 		try {
+			// sends the request
 			await Api.post<void>(saveEndpoint);
+			// dispatches the action
 			dispatch<StarExerciseInSelectionAction>({
 				type: ActionTypes.starExerciseInSelection,
 				payload: { saved: save, exercise: id },
 			});
 		} catch (error) {
-			console.log("Error getting exercises");
+			// do nothing on error
 		}
 	};
 }
