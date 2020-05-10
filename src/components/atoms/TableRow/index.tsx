4f004632643cdbab49301fe15d93c8b804619b74
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 20:24:30 +0000

    added comments to TableRow

--- src/components/atoms/TableRow/index.tsx
+++ src/components/atoms/TableRow/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Row } from "./styles";
 import { IPRops } from "./interface";
-
+/*Table row used for other components encapsulated within it*/
 export const TableRow = ({ children }: IPRops) => {
-	return <Row>{children}</Row>;
+    return <Row>{children}</Row>;
 };
