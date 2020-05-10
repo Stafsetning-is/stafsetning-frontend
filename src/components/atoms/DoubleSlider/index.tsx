16a826a9c9fdf9469af9b2a572f5a4abb0977784
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 14:55:03 +0000

    added comments to DoubleSlider

--- src/components/atoms/DoubleSlider/index.tsx
+++ src/components/atoms/DoubleSlider/index.tsx
@@ -7,49 +7,52 @@ import { ThemeProvider } from "@material-ui/styles";
 import { FormLabel } from "../../";
 import { theme } from "./utils";
 /**
- * Double slider
+ *
+ * @param min describes the minimum value for the DoubleSlider
+ * @param max describes the maximum value for the DoubleSlider
+ * @param defaultValues describes the minimum and maximum default values that the slider starts on
  */
 export const DoubleSlider = ({
-	value: { min, max },
-	defaultValues,
-	onChange,
-	label,
+    value: { min, max },
+    defaultValues,
+    onChange,
+    label,
 }: IProps) => {
-	/**
-	 * Recieves event from material-uui slider
-	 * and calls the onChange callback
-	 */
-	const handleChange = (event: any, newValue: number | number[]) => {
-		if (typeof newValue === "object") {
-			const newVal: DoubleSliderValue = {
-				max: newValue[1],
-				min: newValue[0],
-			};
-			onChange(newVal);
-		}
-	};
+    /**
+     * Recieves event from material-ui slider
+     * and calls the onChange callback
+     */
+    const handleChange = (event: any, newValue: number | number[]) => {
+        if (typeof newValue === "object") {
+            const newVal: DoubleSliderValue = {
+                max: newValue[1],
+                min: newValue[0],
+            };
+            onChange(newVal);
+        }
+    };
+    // valuetext is the bubble that tells the user what the current value of min or max is while changing the values
+    const valuetext = (value: number) => `${value}`;
 
-	const valuetext = (value: number) => `${value}`;
+    const getVal = () => [
+        min < defaultValues.min ? defaultValues.min : min,
+        max > defaultValues.max ? defaultValues.max : max,
+    ];
 
-	const getVal = () => [
-		min < defaultValues.min ? defaultValues.min : min,
-		max > defaultValues.max ? defaultValues.max : max,
-	];
-
-	return (
-		<Outer>
-			<FormLabel text={label} />
-			<ThemeProvider theme={theme}>
-				<Slider
-					max={defaultValues.max}
-					min={defaultValues.min}
-					value={getVal()}
-					onChange={handleChange}
-					valueLabelDisplay="auto"
-					aria-labelledby="range-slider"
-					getAriaValueText={valuetext}
-				/>
-			</ThemeProvider>
-		</Outer>
-	);
+    return (
+        <Outer>
+            <FormLabel text={label} />
+            <ThemeProvider theme={theme}>
+                <Slider
+                    max={defaultValues.max}
+                    min={defaultValues.min}
+                    value={getVal()}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    aria-labelledby="range-slider"
+                    getAriaValueText={valuetext}
+                />
+            </ThemeProvider>
+        </Outer>
+    );
 };
--- src/components/atoms/DoubleSlider/utils.ts
+++ src/components/atoms/DoubleSlider/utils.ts
@@ -1,17 +1,19 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-
+/*
+describes the outlook of the double slider
+*/
 export const theme = createMuiTheme({
-	overrides: {
-		MuiSlider: {
-			thumb: {
-				color: "#6600ff",
-			},
-			track: {
-				color: "#6600ff",
-			},
-			rail: {
-				color: "black",
-			},
-		},
-	},
+    overrides: {
+        MuiSlider: {
+            thumb: {
+                color: "#6600ff",
+            },
+            track: {
+                color: "#6600ff",
+            },
+            rail: {
+                color: "black",
+            },
+        },
+    },
 });
