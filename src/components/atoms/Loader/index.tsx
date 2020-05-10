6f6b795f445441d1c2ae57cec76348c740ee12c5
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 15:24:15 +0000

    added comments to Loader

--- src/components/atoms/Loader/index.tsx
+++ src/components/atoms/Loader/index.tsx
@@ -4,14 +4,14 @@ import { Container } from "./styles";
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
 /**
- * The basic loader animation used throughout the project
- * @param needs loading boolean prop with optional horizontalPadding number prop
+ * The basic loader animation used throughout the project anytime content needs to be loaded to the site
+ * @param loading boolean prop with optional horizontalPadding number prop
  */
 export const Loader = ({ loading, horizontalPadding }: IProps) => {
-	if (!loading) return null;
-	return (
-		<Container>
-			<ClimbingBoxLoader size={20} color={"#00FFAA"} loading={true} />
-		</Container>
-	);
+    if (!loading) return null;
+    return (
+        <Container>
+            <ClimbingBoxLoader size={20} color={"#00FFAA"} loading={true} />
+        </Container>
+    );
 };
