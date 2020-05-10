e93798a2049722f916c821791bdf5250543199e8
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 16:01:30 +0000

    added comments to Trophy

--- src/components/atoms/Shaky/index.tsx
+++ src/components/atoms/Shaky/index.tsx
@@ -3,10 +3,10 @@ import { IProps } from "./interface";
 import { ShakeHorizontal } from "reshake";
 
 /**
- *
  * Component that shakes its children
  */
 export const Shaky = ({ shake, children }: IProps) => {
-	if (shake) return <ShakeHorizontal fixed={true}>{children}</ShakeHorizontal>;
-	else return children;
+    if (shake)
+        return <ShakeHorizontal fixed={true}>{children}</ShakeHorizontal>;
+    else return children;
 };
--- src/components/atoms/Trophy/index.tsx
+++ src/components/atoms/Trophy/index.tsx
@@ -3,15 +3,22 @@ import { Outer, Title, Icon, Inner } from "./styles";
 import { IProps } from "./interface";
 import { TrophyIcon } from "../../";
 
+/**
+ * Trophy is earned when specific requirements set by the trophy creator are met by the user
+ * when the user unlocks the trophy, it will become available for viewing in the trophy area located in the user´s profile page
+ * @param icon has to do with the image shown on the trophy modal
+ * @param title Represents the name of the trophy
+ * @param
+ */
 export const Trophy = ({ icon, title, locked, onClick }: IProps) => {
-	return (
-		<Outer onClick={onClick}>
-			<Inner theme={{ locked }} onClick={onClick}>
-				<Icon>
-					<TrophyIcon icon={icon} />
-				</Icon>
-				<Title>{title}</Title>
-			</Inner>
-		</Outer>
-	);
+    return (
+        <Outer onClick={onClick}>
+            <Inner theme={{ locked }} onClick={onClick}>
+                <Icon>
+                    <TrophyIcon icon={icon} />
+                </Icon>
+                <Title>{title}</Title>
+            </Inner>
+        </Outer>
+    );
 };
