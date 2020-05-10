721cd4a59a0bdf4fee2b53ad83d3019228e85924
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 15:21:05 +0000

    added comments to FormLabel

--- src/components/atoms/FormLabel/index.tsx
+++ src/components/atoms/FormLabel/index.tsx
@@ -2,11 +2,16 @@ import * as React from "react";
 import { IProps } from "./interface";
 import { Container, LabelText, ErrorText } from "./styles";
 
+/**
+ *FormLabel essentially a title that is styled and encapsulated within a container
+ * @param text used as the main title text
+ * @param errorMessage used for error messages
+ */
 export const FormLabel = ({ text, errorMessage }: IProps) => {
-	return (
-		<Container>
-			<LabelText>{text}</LabelText>
-			<ErrorText> {errorMessage}</ErrorText>
-		</Container>
-	);
+    return (
+        <Container>
+            <LabelText>{text}</LabelText>
+            <ErrorText> {errorMessage}</ErrorText>
+        </Container>
+    );
 };
