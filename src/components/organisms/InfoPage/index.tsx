20989d5c14fb08f69a8d003a9ebe5310f7491831
nannalop <natalia17@ru.is>
2020-05-10 16:04:56 +0000

    commented InfoPage

--- src/components/organisms/InfoPage/index.tsx
+++ src/components/organisms/InfoPage/index.tsx
@@ -1,44 +1,48 @@
 import React from "react";
 import {
-	Outer,
-	MessageContainer,
-	IconContainer,
-	TextContainer,
-	TitleContainer,
-	ParagraphContainer,
+    Outer,
+    MessageContainer,
+    IconContainer,
+    TextContainer,
+    TitleContainer,
+    ParagraphContainer
 } from "./styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faTrophy,
-	faExclamationTriangle,
-	faFrog,
+    faTrophy,
+    faExclamationTriangle,
+    faFrog
 } from "@fortawesome/free-solid-svg-icons";
 import { IProps } from "./interface";
-
+/**
+ * A component that shows up whenever there is something
+ * missing or if there's an error in a page so the
+ * user knows what's going on
+ */
 export const InfoPage = ({ text, title, type, children }: IProps) => {
-	const icon = (() => {
-		switch (type) {
-			case "error":
-				return faExclamationTriangle;
-			case "not-found":
-				return faFrog;
-			case "success":
-				return faTrophy;
-		}
-	})();
+    const icon = (() => {
+        switch (type) {
+            case "error":
+                return faExclamationTriangle;
+            case "not-found":
+                return faFrog;
+            case "success":
+                return faTrophy;
+        }
+    })();
 
-	return (
-		<Outer>
-			<MessageContainer>
-				<IconContainer>
-					<FontAwesomeIcon icon={icon} />
-				</IconContainer>
-				<TextContainer>
-					<TitleContainer>{title}</TitleContainer>
-					<ParagraphContainer>{text}</ParagraphContainer>
-				</TextContainer>
-			</MessageContainer>
-			{children ?? null}
-		</Outer>
-	);
+    return (
+        <Outer>
+            <MessageContainer>
+                <IconContainer>
+                    <FontAwesomeIcon icon={icon} />
+                </IconContainer>
+                <TextContainer>
+                    <TitleContainer>{title}</TitleContainer>
+                    <ParagraphContainer>{text}</ParagraphContainer>
+                </TextContainer>
+            </MessageContainer>
+            {children ?? null}
+        </Outer>
+    );
 };
