50ec810deadfa74a4447c3bac5b44bcc47fa943a
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 13:58:50 +0000

    added comments to CheckBoxinput

--- src/components/atoms/CheckBoxInput/index.tsx
+++ src/components/atoms/CheckBoxInput/index.tsx
@@ -4,33 +4,33 @@ import { FormLabel } from "..";
 import Toggle from "react-toggle";
 import { ToggleWrapper } from "./styles";
 /**
- * Password input that takes in value and onChange function
- * This is for the form functionality
+ * BoxInput that takes in value and onChange function
+ * This is for the Form functionality
  */
 export const CheckBoxInput = ({
-	value,
-	onChange,
-	label,
-	placeholder,
-	validationMessage,
+    value,
+    onChange,
+    label,
+    placeholder,
+    validationMessage,
 }: InputComponent) => {
-	/**
-	 * Returns validation message
-	 */
-	const getValidationMessage = () => {
-		if (!value) return "";
-		return validationMessage ?? "";
-	};
-
-	return (
-		<React.Fragment>
-			<FormLabel text={label} errorMessage={getValidationMessage()} />
-			<ToggleWrapper>
-				<Toggle
-					defaultChecked={value}
-					onChange={(e) => onChange(e.target.checked)}
-				/>
-			</ToggleWrapper>
-		</React.Fragment>
-	);
+    /**
+     * Returns validation message depending on if there is a value or not
+     */
+    const getValidationMessage = () => {
+        if (!value) return "";
+        return validationMessage ?? "";
+    };
+    /*returns the validation message be it an error or success message within the form*/
+    return (
+        <React.Fragment>
+            <FormLabel text={label} errorMessage={getValidationMessage()} />
+            <ToggleWrapper>
+                <Toggle
+                    defaultChecked={value}
+                    onChange={(e) => onChange(e.target.checked)}
+                />
+            </ToggleWrapper>
+        </React.Fragment>
+    );
 };
