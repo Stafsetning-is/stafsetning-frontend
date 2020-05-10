4f39f0cd0d8878899c31081f0ed81ec82fa6246d
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 15:39:30 +0000

    added comments to Points

--- src/components/atoms/Points/index.tsx
+++ src/components/atoms/Points/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Outer, Points as Text, Coin } from "./styles";
 import { IProps } from "./interface";
 
+/**
+ * This has to do with the award system and how it is displayed on the site
+ * @param points has to do with the number of coins that the user has earned
+ */
 export const Points = ({ points, whiteBG }: IProps) => {
-	return (
-		<Outer>
-			<Coin />
-			<Text theme={{ darkText: !!whiteBG }}>{points}</Text>
-		</Outer>
-	);
+    //here the coin is displayed along with the number of them that the user has earned
+    return (
+        <Outer>
+            <Coin />
+            <Text theme={{ darkText: !!whiteBG }}>{points}</Text>
+        </Outer>
+    );
 };
