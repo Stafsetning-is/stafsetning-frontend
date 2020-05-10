e31c189e2112139281b07008c3403f980cb8d6e0
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2020-05-10 21:03:10 +0000

    added comments to FlipCard

--- src/components/atoms/FlipCard/index.tsx
+++ src/components/atoms/FlipCard/index.tsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Outer } from "./styles";
 import { IProps } from "./interface";
 
+/*FlipCard is used for the display of Trophies
+ in regards to the pop-up for when the user earns the trophy
+ They are set to be shown when the setSow is set to true
+ */
 export const FlipCard = ({ children }: IProps) => {
-	const [show, setShow] = useState(false);
+    const [show, setShow] = useState(false);
 
-	useEffect(() => {
-		setTimeout(() => setShow(true), 250);
-	}, []);
+    useEffect(() => {
+        setTimeout(() => setShow(true), 250);
+    }, []);
 
-	return <Outer theme={{ show }}>{children}</Outer>;
+    return <Outer theme={{ show }}>{children}</Outer>;
 };
