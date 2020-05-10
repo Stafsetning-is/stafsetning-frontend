676ea75aa5a8fe125b87c2082f8f3be0f8414e2c
nannalop <natalia17@ru.is>
2020-05-10 15:45:12 +0000

    commented FrontPage

--- src/components/organisms/FrontPageView/ConnectedUsers/index.tsx
+++ src/components/organisms/FrontPageView/ConnectedUsers/index.tsx
@@ -4,28 +4,34 @@ import { IProps } from "./interface";
 import { Inner, Outer, Text } from "./styles";
 import UserIcon from "../UserIcon";
 
+/**
+ * Holds the number of connected users in the system.
+ * Makes sure to change to the correct Icelandic conjugation
+ * if there is one user connected.
+ * Uses the UserIcon component to show the picture.
+ */
 export default ({ connectedUsers }: IProps) => {
-	const users = connectedUsers.slice(0, 3);
+    const users = connectedUsers.slice(0, 3);
 
-	const getPluralOrSingular = (count: number) => {
-		if (count === 11 || count % 10 !== 1) return "nemendur";
-		return "nemandi";
-	};
+    const getPluralOrSingular = (count: number) => {
+        if (count === 11 || count % 10 !== 1) return "nemendur";
+        return "nemandi";
+    };
 
-	return (
-		<Outer>
-			<InfoBox>
-				<Inner>
-					{users.map((user, i) => (
-						<UserIcon index={i} src={user.avatar} />
-					))}
-					<Text theme={{ count: users.length }}>{`${
-						connectedUsers.length
-					} ${getPluralOrSingular(
-						connectedUsers.length
-					)} á Stafsetning.is ákkurat núna`}</Text>
-				</Inner>
-			</InfoBox>
-		</Outer>
-	);
+    return (
+        <Outer>
+            <InfoBox>
+                <Inner>
+                    {users.map((user, i) => (
+                        <UserIcon index={i} src={user.avatar} />
+                    ))}
+                    <Text theme={{ count: users.length }}>{`${
+                        connectedUsers.length
+                    } ${getPluralOrSingular(
+                        connectedUsers.length
+                    )} á Stafsetning.is ákkurat núna`}</Text>
+                </Inner>
+            </InfoBox>
+        </Outer>
+    );
 };
--- src/components/organisms/FrontPageView/index.tsx
+++ src/components/organisms/FrontPageView/index.tsx
@@ -9,31 +9,33 @@ import { Title, Paragraph } from "./styles";
 import { AuthHider } from "../../../hoc";
 
 /**
- * View that renders all components
- * that should display on front page
+ * A view that renders all components
+ * that should display on the front page
+ * Consists of an introduction text, the number of connected users,
+ * the filter and the exercises.
  */
 const View = ({ filterSettings, exercises, connectedUsers }: IProps) => {
-	const filtered = filterExercises(exercises, filterSettings);
+    const filtered = filterExercises(exercises, filterSettings);
 
-	return (
-		<React.Fragment>
-			<AuthHider setAuthLevel="guest">
-				<React.Fragment>
-					<Title>Stafsetningaræfingar</Title>
-					<Paragraph>{MAIN_TEXT}</Paragraph>
-				</React.Fragment>
-			</AuthHider>
-			<ConnectedUsers connectedUsers={connectedUsers} />
-			<FilterSearch />
-			<ExerciseBoxesContainer exercises={filtered} />
-		</React.Fragment>
-	);
+    return (
+        <React.Fragment>
+            <AuthHider setAuthLevel="guest">
+                <React.Fragment>
+                    <Title>Stafsetningaræfingar</Title>
+                    <Paragraph>{MAIN_TEXT}</Paragraph>
+                </React.Fragment>
+            </AuthHider>
+            <ConnectedUsers connectedUsers={connectedUsers} />
+            <FilterSearch />
+            <ExerciseBoxesContainer exercises={filtered} />
+        </React.Fragment>
+    );
 };
 
 const mapStateToProps = (store: StoreState) => ({
-	filterSettings: store.filter,
-	exercises: store.exercises.selection,
-	connectedUsers: store.socket.connectedUsers,
+    filterSettings: store.filter,
+    exercises: store.exercises.selection,
+    connectedUsers: store.socket.connectedUsers
 });
 
 export const FrontPageView = connect(mapStateToProps)(View);
