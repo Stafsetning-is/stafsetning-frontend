74b0afbedff542188f4b8a843cd10f066270f3ed
Júlía <juliai17@ru.is>
2020-05-12 18:51:16 +0000

    cwt

--- src/components/organisms/SettingsPage/index.tsx
+++ src/components/organisms/SettingsPage/index.tsx
@@ -15,32 +15,32 @@ import { StoreState } from "../../../reducers";
  * It stores the difficulty changes in a redux store.
  */
 const Component = ({ changeUserDifficulty, difficulty }: IProps) => {
-    return (
-        <Outer>
-            <LiveForm<DifficultyForm>
-                fields={changeDifficultyForm}
-                postTo="/api/v1/users/difficulty"
-                onSuccess={({ difficulty }) => {
-                    changeUserDifficulty(difficulty);
-                }}
-                label="Erfiðleikastig"
-                defaultValues={{ difficulty }}
-            />
-            <PasswordTitle>Breyta um Lykilorð</PasswordTitle>
-            <FormGenerator
-                fields={changePasswordForm}
-                label="Breyta um lykilorð"
-                onSuccess={({}) => {}}
-                postTo="/api/v1/users/password"
-            />
+	return (
+		<Outer>
+			<LiveForm<DifficultyForm>
+				fields={changeDifficultyForm}
+				postTo="/api/v1/users/difficulty"
+				onSuccess={({ difficulty }) => {
+					changeUserDifficulty(difficulty);
+				}}
+				label="Námsstig"
+				defaultValues={{ difficulty }}
+			/>
+			<PasswordTitle>Lykilorð</PasswordTitle>
+			<FormGenerator
+				fields={changePasswordForm}
+				label="Breyta lykilorði"
+				onSuccess={({}) => {}}
+				postTo="/api/v1/users/password"
+			/>
 
-            <AccessibilitySettings label="Aðgengisstillingar" />
-        </Outer>
-    );
+			<AccessibilitySettings label="Stillingar í æfingu" />
+		</Outer>
+	);
 };
 
 const mapStateToProps = (state: StoreState) => state.auth.user;
 
 export const SettingsPage = connect(mapStateToProps, {
-    changeUserDifficulty
+	changeUserDifficulty,
 })(Component);
