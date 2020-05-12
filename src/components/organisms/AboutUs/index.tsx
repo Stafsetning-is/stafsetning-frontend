fde512395d53a5a227cddf936032b8e1c0513907
Júlía <juliai17@ru.is>
2020-05-12 18:50:34 +0000

    cwt

--- src/components/organisms/AboutUs/index.tsx
+++ src/components/organisms/AboutUs/index.tsx
@@ -19,11 +19,11 @@ export const AboutPage = () => {
 			<Content>
 				Stafsetning.is er kennsluvefur ætlaður nemendum í grunn- og
 				framhaldsskóla til að þjálfa sig í stafsetningu. Verkefnið var
-				unnið sem lokaverkefni til BS gráðu í tölvunarfræði við
+				unnið sem lokaverkefni til BSc gráðu í tölvunarfræði við
 				Háskólann í Reykjavík. Markmið okkar með síðunni er að færa
-				stafsetningarkennslu frá kennslustofunni og yfir á rafrænt form.
-				Mikil áhersla var lögð á notendaupplifun og því er hún hönnuð
-				með notkun allra nemenda og allra aldurshópa í huga.
+				stafsetningarkennslu frá kennslustofunni yfir á rafrænt form.
+				Mikil áhersla var lögð á notandaupplifun og því er hún hönnuð
+				með notkun allra nemenda og aldurshópa í huga.
 			</Content>
 			<Content>Þróunaraðilar:</Content>
 			<DeveloperImageWrapper>
