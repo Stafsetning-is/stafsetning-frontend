29e0ed7f33a2046d742aa238d7fedaa9e6da460a
Júlía <juliai17@ru.is>
2020-05-12 23:14:02 +0000

    cookie page styles

--- src/components/pages/Front/CookieInfoPage/styles.ts
+++ src/components/pages/Front/CookieInfoPage/styles.ts
@@ -1,20 +1,27 @@
 import styled from "styled-components";
 
 export const MainHeading = styled.h1`
-    margin: 25px 0 20px 0;
+	margin: 25px 0 20px 0;
+	color: #333;
 `;
 
 export const Heading = styled.h2`
-    margin: 15px 0 10px 0;
+	margin: 15px 0 10px 0;
 `;
 
-export const Content = styled.p``;
+export const Content = styled.p`
+	margin-bottom: 40px;
+	line-height: 35px;
+	color: #666;
+	font-size: 20px;
+	font-width: 400;
+`;
 
 export const Mailto = styled.a`
-    margin-left: 5px;
-    color: #0000ee;
-    text-decoration: underline;
-    :visited {
-        color: #551a8b;
-    }
+	margin-left: 5px;
+	color: #0000ee;
+	text-decoration: underline;
+	:visited {
+		color: #551a8b;
+	}
 `;
