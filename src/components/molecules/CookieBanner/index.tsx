f60b0d9f4d2780d24a46697d7622bf6d9eaaac59
gabriels17 <gabriel.sighvatsson@gmail.com>
2020-05-14 18:06:47 +0000

    Removed unused deps

--- src/components/molecules/CookieBanner/index.tsx
+++ src/components/molecules/CookieBanner/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCookieBite } from "@fortawesome/free-solid-svg-icons";
-import { IconWrapper, Link, Comment } from "./styles";
+import { Link, Comment } from "./styles";
 import { BANNER_PROPS, CONTENT_TEXT, DETAILS_TEXT } from "./utils";
 import CookieConsent from "react-cookie-consent";
 import { NavLink } from "react-router-dom";
@@ -11,14 +9,14 @@ import { NavLink } from "react-router-dom";
  * accept the usage of cookies and links to policy page.
  */
 export const CookieBanner = () => {
-	return (
-		<CookieConsent {...BANNER_PROPS}>
-			<div>
-				<Comment>{CONTENT_TEXT}</Comment>
-				<NavLink to="/cookies">
-					<Link>{DETAILS_TEXT}</Link>
-				</NavLink>
-			</div>
-		</CookieConsent>
-	);
+    return (
+        <CookieConsent {...BANNER_PROPS}>
+            <div>
+                <Comment>{CONTENT_TEXT}</Comment>
+                <NavLink to="/cookies">
+                    <Link>{DETAILS_TEXT}</Link>
+                </NavLink>
+            </div>
+        </CookieConsent>
+    );
 };
